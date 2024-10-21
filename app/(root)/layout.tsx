8ea4453184b8194dc85157c64d6d0aea69e461b0
Mehdi-ehsani <metti.work@gmail.com>
2024-10-21 22:54:17 +0330

    fix tailwind bugs

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const Layout = () => {
   return (
-    <main className='background-light850_dark100 relative'>
+    <main className='background-light850_dark100 text-light400_light500 relative'>
         Navbar
+        <p className='text-primary-500'>Metti's test</p>
         <div className='flex'>
             leftsidebar
 
