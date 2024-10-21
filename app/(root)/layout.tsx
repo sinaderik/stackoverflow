d182a7eb7d6e9615cadc6965d8c715e0c90b5bc1
Sina <sinaderik7@gmail.com>
2024-10-21 18:32:33 +0330

    layout component is created within the root folder

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Layout = () => {
+  return (
+    <main className='background-light850_dark100 relative'>
+        Navbar
+        <div className='flex'>
+            leftsidebar
+
+            rightsidebar
+        </div>
+    </main>
+  )
+}
+
+export default Layout
