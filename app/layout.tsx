e8fdb21554fdf031a8cf04f7e5957d246ae9daa5
Sina <sinaderik7@gmail.com>
2024-10-21 17:47:01 +0330

    small change

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,7 +47,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+    appearance={{
+      elements:{
+        formButtonPrimary:"primary-gradient",
+        footerActionLink:"primary-text-gradient hover:text-primary-500"
+      }
+    }}
+    >
       <html lang="en">
         <body
           className={`${inter.variable} ${spaceGrotesk.variable}`}
