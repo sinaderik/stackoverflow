36e218716eded667c704363a6a144f52024697ce
Sina <sinaderik7@gmail.com>
2024-10-24 16:04:53 +0330

    leftSidebar component is created and basic styles are added

---
+++ components/shared/leftSideBar/LeftSideBar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LeftSideBar = () => {
+  return (
+    <div className='z-50 border-2 h-screen mt-[104px] max-lg:mt-[88px]'>
+        <p className='h2-bold text-dark100_light900'>LeftSideBar</p>
+    </div>
+  )
+}
+
+export default LeftSideBar
