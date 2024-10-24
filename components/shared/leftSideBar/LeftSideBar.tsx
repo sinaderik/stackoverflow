5b86765cbc526cb032f3d74a343967db1d625a92
Sina <sinaderik7@gmail.com>
2024-10-24 16:33:48 +0330

    links are renderd and left sidebar stays on the left side

--- components/shared/leftSideBar/LeftSideBar.tsx
+++ components/shared/leftSideBar/LeftSideBar.tsx
@@ -1,11 +1,53 @@
-import React from 'react'
+"use client"
+
+import { sidebarLinks } from "@/constants";
+import { usePathname } from "next/navigation";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { SignedOut } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 const LeftSideBar = () => {
-  return (
-    <div className='z-50 border-2 h-screen mt-[104px] max-lg:mt-[88px]'>
-        <p className='h2-bold text-dark100_light900'>LeftSideBar</p>
-    </div>
-  )
-}
+  const pathname = usePathname();
 
-export default LeftSideBar
+  return (
+    <div className="custom-scrollbar background-light900_dark200 light-border sticky left-0 top-0 flex h-screen w-fit flex-col justify-between  overflow-y-auto border-r p-6 pt-36 shadow-light-300 dark:shadow-none max-sm:hidden lg:w-[266px]">
+      {sidebarLinks.map((item) => {
+        const isActive =
+          (pathname.includes(item.route) && item.route.length > 1) ||
+          pathname === item.route;
+        return (
+            <Link
+              className={`${
+                isActive
+                  ? "primary-gradient rounded-lg text-light-900"
+                  : "text-dark300_light900"
+              } 
+                flex items-center justify-start bg-transparent gap-4 p-4
+            `}
+              href={item.route}
+            >
+              <Image
+                src={item.imgURL}
+                alt={item.label}
+                width={20}
+                height={20}
+                className={`${isActive ? "" : "invert-colors"}`}
+              />
+              <p className={`${isActive ? "base-bold" : "base-medium"}`}>
+                {item.label}
+              </p>
+            </Link>
+        );
+      })}
+    </div>
+  );
+};
+
+export default LeftSideBar;
