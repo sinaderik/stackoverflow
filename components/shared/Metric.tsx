a5bf14ae68c2bc7d90baccba4421d9658221d668
Sina <sinaderik7@gmail.com>
2024-10-29 17:52:58 +0330

    Metric component is completed

--- components/shared/Metric.tsx
+++ components/shared/Metric.tsx
@@ -1,4 +1,6 @@
+"use client"
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 type MetricProps = {
@@ -20,21 +22,37 @@ const Metric = ({
   textStyles,
   isAuthor,
 }: MetricProps) => {
-  return (
-    <div className="flex-center flex-wrap gap-1">
-      <Image
-        src={imgUrl}
-        width={16}
-        height={16}
-        alt={alt}
-        className={`object-contain ${href ? "rounded-full invert-colors" : ""}`}
-      />
-      <p className={`${textStyles} flex items-center gap-1`}>
-        {value}
-        {title}
-      </p>
-    </div>
-  );
+  const metricContent = () => {
+    return (
+      <>
+        <Image
+          src={imgUrl}
+          width={16}
+          height={16}
+          alt={alt}
+          className={`object-contain ${
+            href ? "rounded-full invert-colors" : ""
+          }`}
+        />
+        <p className={`${textStyles} flex items-center gap-1`}>
+          {value}
+          <span
+            className={`small-regular line-clamp-1 ${
+              isAuthor ? "max-sm:hidden" : ""
+            }`}
+          >
+            {title}
+          </span>
+        </p>
+      </>
+    );
+  };
+
+  if (href) {
+    <Link href={href} className="flex-center gap-1">{metricContent()}</Link>;
+  }
+
+  return <div className="flex-center flex-wrap gap-1">{metricContent()}</div>;
 };
 
 export default Metric;
