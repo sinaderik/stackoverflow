9d7f4a9da2fa08fb9c2f81c3bf3ac75116e5a93d
Sina <sinaderik7@gmail.com>
2024-10-29 21:40:57 +0330

    route for the ask-question page is changed

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const AskQuestion = () => {
     return (
-        <div>
-            
+        <div className='text-dark400_light900'>
+            AskQuestion
         </div>
     );
 };
