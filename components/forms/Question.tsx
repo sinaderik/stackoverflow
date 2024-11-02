80f09bbe03c1c78504abb822860f120f3f05931c
Sina <sinaderik7@gmail.com>
2024-11-02 13:18:15 +0330

    rest of the form field are added to the form

--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -47,11 +47,46 @@ const Question = () => {
               <FormLabel className="paragraph-semibold text-dark400_light800">Title<span className="text-primary-500 ml-1">*</span></FormLabel>
               <FormControl className="mt-3.5">
                 <Input
-                className="no-focus paragraph-regular background-light700_dark300 text-dark300_light700 light-border-2 border min-h-[56px]"
+                className="no-focus paragraph-regular background-light900_dark300 text-dark300_light700 light-border-2 border min-h-[56px]"
                 {...field} />
               </FormControl>
               <FormDescription className="body-regular mt-2.5 text-light-500">
                 Be specific and imagine you're asking a question to another person
+              </FormDescription>
+              <FormMessage className="text-red-500" />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="explanation"
+          render={({ field }) => (
+            <FormItem className="flex flex-col w-full">
+              <FormLabel className="paragraph-semibold text-dark400_light800">Detailed explanation of your problem<span className="text-primary-500 ml-1">*</span></FormLabel>
+              <FormControl className="mt-3.5">
+                {/* add an editor form component */}
+              </FormControl>
+              <FormDescription className="body-regular mt-2.5 text-light-500">
+                Introduce the problem and expand on what you put in the title. Minimun 10 characters
+              </FormDescription>
+              <FormMessage className="text-red-500" />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="tags"
+          render={({ field }) => (
+            <FormItem className="flex flex-col w-full">
+              <FormLabel className="paragraph-semibold text-dark400_light800">Tags<span className="text-primary-500 ml-1">*</span></FormLabel>
+              <FormControl className="mt-3.5">
+                <Input
+                placeholder="Add tags..."
+                className="no-focus paragraph-regular background-light900_dark300 text-dark300_light700 light-border-2 border min-h-[56px]"
+                {...field} />
+              </FormControl>
+              <FormDescription className="body-regular mt-2.5 text-light-500">
+                Add up to 3 tags to describe what your question is about. You need to press enter to add a tag
               </FormDescription>
               <FormMessage className="text-red-500" />
             </FormItem>
--- lib/validations.ts
+++ lib/validations.ts
@@ -4,5 +4,5 @@ import { z } from "zod";
 export const QuestionsSchema = z.object({
     title: z.string().min(5).max(130),
     explanation:z.string().min(10).min(100),
-    tags:z.array(z.string().min(1).max(15)).min(1).max(3),
+    tags:z.array(z.string().min(1).max(15)).min(1,{ message: "Must contain at least 1 tag" }).max(3,{message:"Must contain 3 tags exactly"}),
   });
