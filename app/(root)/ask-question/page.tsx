31be69a6228819ad5d5bc4f421f829c37b853550
Sina <sinaderik7@gmail.com>
2024-11-02 12:04:24 +0330

    question from is initialized using zod and react hook form from shadcn

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,9 +1,13 @@
+import Question from '@/components/forms/Question';
 import React from 'react';
 
 const AskQuestion = () => {
     return (
         <div className='text-dark400_light900'>
-            AskQuestion
+            <h1 className='h1-bold text-dark100_light900'>Ask a question</h1>
+            <div className='mt-9'>
+            <Question />
+            </div>
         </div>
     );
 };
