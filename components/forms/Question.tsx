272f614b92c91918528e39d4ad904eb37013408a
Sina <sinaderik7@gmail.com>
2024-11-02 12:37:32 +0330

    question schema is implemented

--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -1,8 +1,8 @@
 "use client";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
 import { z } from "zod";
-import { Button } from "@/components/ui/button"
 import {
   Form,
   FormControl,
@@ -13,24 +13,22 @@ import {
   FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
+import { QuestionsSchema } from "@/lib/validations";
 
 
 
-const formSchema = z.object({
-  username: z.string().min(2).max(50),
-});
 
 const Question = () => {
   // 1. Define your form.
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const form = useForm<z.infer<typeof QuestionsSchema>>({
+    resolver: zodResolver(QuestionsSchema),
     defaultValues: {
-      username: "",
+      title: "",
     },
   });
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  function onSubmit(values: z.infer<typeof QuestionsSchema>) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
     console.log(values);
@@ -41,10 +39,10 @@ const Question = () => {
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
         <FormField
           control={form.control}
-          name="username"
+          name="title"
           render={({ field }) => (
             <FormItem>
-              <FormLabel>Username</FormLabel>
+              <FormLabel>Title</FormLabel>
               <FormControl>
                 <Input placeholder="shadcn" {...field} />
               </FormControl>
