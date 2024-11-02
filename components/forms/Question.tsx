74261b1c0fcf586d1b2b89eabd97f9c82b27d3c8
Sina <sinaderik7@gmail.com>
2024-11-02 20:44:17 +0330

    a text editor added to the form using TinyMCE package

--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -11,21 +11,21 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 import { QuestionsSchema } from "@/lib/validations";
-
-
-
+import { useRef } from "react";
+import { Editor } from "@tinymce/tinymce-react";
 
 const Question = () => {
+  const editorRef = useRef(null);
 
   const form = useForm<z.infer<typeof QuestionsSchema>>({
     resolver: zodResolver(QuestionsSchema),
     defaultValues: {
       title: "",
-      explanation:"",
-      tags:[],
+      explanation: "",
+      tags: [],
     },
   });
 
@@ -38,20 +38,27 @@ const Question = () => {
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col w-full gap-10">
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex flex-col w-full gap-10"
+      >
         <FormField
           control={form.control}
           name="title"
           render={({ field }) => (
             <FormItem className="flex flex-col w-full">
-              <FormLabel className="paragraph-semibold text-dark400_light800">Title<span className="text-primary-500 ml-1">*</span></FormLabel>
+              <FormLabel className="paragraph-semibold text-dark400_light800">
+                Title<span className="text-primary-500 ml-1">*</span>
+              </FormLabel>
               <FormControl className="mt-3.5">
                 <Input
-                className="no-focus paragraph-regular background-light900_dark300 text-dark300_light700 light-border-2 border min-h-[56px]"
-                {...field} />
+                  className="no-focus paragraph-regular background-light900_dark300 text-dark300_light700 light-border-2 border min-h-[56px]"
+                  {...field}
+                />
               </FormControl>
               <FormDescription className="body-regular mt-2.5 text-light-500">
-                Be specific and imagine you're asking a question to another person
+                Be specific and imagine you're asking a question to another
+                person
               </FormDescription>
               <FormMessage className="text-red-500" />
             </FormItem>
@@ -62,12 +69,50 @@ const Question = () => {
           name="explanation"
           render={({ field }) => (
             <FormItem className="flex flex-col w-full">
-              <FormLabel className="paragraph-semibold text-dark400_light800">Detailed explanation of your problem<span className="text-primary-500 ml-1">*</span></FormLabel>
+              <FormLabel className="paragraph-semibold text-dark400_light800">
+                Detailed explanation of your problem
+                <span className="text-primary-500 ml-1">*</span>
+              </FormLabel>
               <FormControl className="mt-3.5">
-                {/* add an editor form component */}
+                <Editor
+                  apiKey={process.env.NEXT_PUBLIC_TINY_EDITOR_API_KEY}
+                  onInit={(_evt, editor)=>{
+                    // @ts-ignore
+                    (editorRef.current = editor)
+                  }}
+                  initialValue=""
+                  init={{
+                    height: 350,
+                    menubar: false,
+                    plugins: [
+                      "advlist",
+                      "autolink",
+                      "lists",
+                      "link",
+                      "image",
+                      "charmap",
+                      "preview",
+                      "anchor",
+                      "searchreplace",
+                      "visualblocks",
+                      "codesample",
+                      "fullscreen",
+                      "insertdatetime",
+                      "media",
+                      "table",
+                    ],
+                    toolbar:
+                      "undo redo | blocks | " +
+                      "codesample | bold italic forecolor | alignleft aligncenter " +
+                      "alignright alignjustify | bullist numlist",
+                    content_style:
+                      "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+                  }}
+                />
               </FormControl>
               <FormDescription className="body-regular mt-2.5 text-light-500">
-                Introduce the problem and expand on what you put in the title. Minimun 10 characters
+                Introduce the problem and expand on what you put in the title.
+                Minimun 10 characters
               </FormDescription>
               <FormMessage className="text-red-500" />
             </FormItem>
@@ -78,15 +123,19 @@ const Question = () => {
           name="tags"
           render={({ field }) => (
             <FormItem className="flex flex-col w-full">
-              <FormLabel className="paragraph-semibold text-dark400_light800">Tags<span className="text-primary-500 ml-1">*</span></FormLabel>
+              <FormLabel className="paragraph-semibold text-dark400_light800">
+                Tags<span className="text-primary-500 ml-1">*</span>
+              </FormLabel>
               <FormControl className="mt-3.5">
                 <Input
-                placeholder="Add tags..."
-                className="no-focus paragraph-regular background-light900_dark300 text-dark300_light700 light-border-2 border min-h-[56px]"
-                {...field} />
+                  placeholder="Add tags..."
+                  className="no-focus paragraph-regular background-light900_dark300 text-dark300_light700 light-border-2 border min-h-[56px]"
+                  {...field}
+                />
               </FormControl>
               <FormDescription className="body-regular mt-2.5 text-light-500">
-                Add up to 3 tags to describe what your question is about. You need to press enter to add a tag
+                Add up to 3 tags to describe what your question is about. You
+                need to press enter to add a tag
               </FormDescription>
               <FormMessage className="text-red-500" />
             </FormItem>
@@ -95,7 +144,7 @@ const Question = () => {
         <Button type="submit">Submit</Button>
       </form>
     </Form>
-  )
+  );
 };
 
 export default Question;
