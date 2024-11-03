9a4db67b5954c744ace2d175ec2a95fd6a65a850
Sina <sinaderik7@gmail.com>
2024-11-03 20:21:59 +0330

    UserSchema is created based on the IUser interface

--- database/user.model.ts
+++ database/user.model.ts
@@ -15,7 +15,20 @@ export interface IUser extends Document {
   joinedAt: Date;
 }
 
-const UserSchema = new Schema({});
+const UserSchema = new Schema({
+  clerkId: { type: String, required: true, unique: true },
+  name: { type: String, required: true },
+  username: { type: String, required: true, unique: true }, 
+  email: { type: String, required: true, unique: true }, 
+  password: { type: String }, 
+  bio: { type: String },
+  picture: { type: String, required: true },
+  location: { type: String },
+  portfolioWebsite: { type: String },
+  reputation: { type: Number, default: 0 }, 
+  saved: [{ type: Schema.Types.ObjectId, ref: "Question" }], 
+  joinedAt: { type: Date, default: Date.now },
+});
 
 const User = models.User || model("User", UserSchema);
 
