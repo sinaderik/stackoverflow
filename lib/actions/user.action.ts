20438d6b65e09458fb7ec12febcc7facac3c3991
Sina <sinaderik7@gmail.com>
2024-11-04 22:47:48 +0330

    createUser function is implemented to create user in the database

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -1,10 +1,23 @@
 "use server";
 
 import User from "@/database/user.model";
+import { connectToDatabase } from "../mongoose";
+import { CreateUserParams } from "./shared.types";
 
+export const createUser = async (userData:CreateUserParams) => {
+  try {
+    connectToDatabase();
+    const newUser=await User.create(userData)
+    return newUser;
+
+  } catch (error) {
+    console.log(error)
+    throw error
+  }
+};
 export const getUserById = async (params:any) => {
   try {
-    
+    connectToDatabase();
     const { userId } = params;
     const user = await User.findOne({ clerkId: userId });
     return user
